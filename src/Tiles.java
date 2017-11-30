49ff69baba7c190f5fc2339eac74172918c972ba
Zachary Reese <31252377+zr00555@users.noreply.github.com>
2017-11-29 21:59:45 -0500

    Add files via upload

---
+++ src/Tiles.java
@@ -0,0 +1,25 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Tiles extends Rectangle{
+	
+	private Piece piece;
+	
+	public Piece getPiece() {
+		return piece;
+	}
+
+	public void setPiece(Piece piece) {
+		this.piece = piece;
+	}
+
+	public Tiles(boolean color, int x, int y) {
+		setWidth(HalmaGame.tileSize);
+		setHeight(HalmaGame.tileSize);
+		
+		relocate((x * HalmaGame.tileSize), (y * HalmaGame.tileSize));
+		
+		setFill(color ? Color.ANTIQUEWHITE : Color.DARKSEAGREEN);
+	}
+
+}
